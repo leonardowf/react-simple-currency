bfb8bca46c27c5032b9118dd0536cc90adec665f
leonardo031195 <leonardocardosodealmeida@gmail.com>
2018-01-04 17:49:53 -0200

    feat(props): Allow to use input html attributes
    Now, you can use maxlength, name and placeholder properties.

--- src/SimpleCurrencyInput.js
+++ src/SimpleCurrencyInput.js
@@ -96,7 +96,7 @@ class SimpleCurrencyInput extends React.Component {
   render() {
     return (
       <input
-				id={this.props.id}
+	id={this.props.id}
         className={this.props.className}
         onBlur={this.props.onInputBlur}
         onFocus={this.props.onInputFocus}
@@ -105,7 +105,9 @@ class SimpleCurrencyInput extends React.Component {
         disabled={this.props.disabled}
         autoFocus={this.props.autoFocus}
         tabIndex={this.state.tabIndex}
-        readOnly={this.state.readOnly}
+        placeholder={this.props.placeholder}
+        name={this.props.name}
+        maxlength={this.props.maxlength}
       />
     )
   }
@@ -132,6 +134,9 @@ SimpleCurrencyInput.propTypes = {
   autoFocus: PropTypes.bool,
   delimiter: PropTypes.string,
   disabled: PropTypes.bool,
+  maxlength: PropTypes.number,
+  placeholder: PropTypes.string,
+  name: PropTypes.string,
   onInputChange: PropTypes.func,
   onInputBlur: PropTypes.func,
   onInputFocus: PropTypes.func,
@@ -153,7 +158,10 @@ SimpleCurrencyInput.defaultProps = {
   autoFocus: false,
   onInputChange: () => {},
   onInputBlur: () => {},
-  onInputFocus: () => {}
+  onInputFocus: () => {},
+  maxlength: '',
+  name: '',
+  placeholder: ''
 }
 
 export default SimpleCurrencyInput
